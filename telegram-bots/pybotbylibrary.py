d2d92e2d0d1650cb8313f9419b3f874665504177
iMshaLch <76241077+iMshaLch@users.noreply.github.com>
2023-04-05 14:04:43 +0500

    Update pybotbylibrary.py

--- telegram-bots/pybotbylibrary.py
+++ telegram-bots/pybotbylibrary.py
@@ -15,18 +15,21 @@ from telegram.ext import (
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
 from bs4 import BeautifulSoup
 
+buttons = ReplyKeyboardMarkup([["Dollar kursi"], ["Hozirgi vaqt"]], 
+    resize_keyboard=True, input_field_placeholder="Nima kerak")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    reply_keyboard = [["Dollar kursi", "Hozirgi vaqt"]]
+    # reply_keyboard = [["Dollar kursi", "Hozirgi vaqt"]]
 
-    await update.message.reply_text(
-        f"Salom {update.effective_user.first_name} !\n\n"
-        "Pastdagi buyruqlardan birini tanlang",
-        reply_markup=ReplyKeyboardMarkup(
-            reply_keyboard, one_time_keyboard=True, resize_keyboard=True, input_field_placeholder="Nima kerak"
-        ),
-    )
-    # await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+    # await update.message.reply_text(
+    #     f"Salom {update.effective_user.first_name} !\n\n"
+    #     "Pastdagi buyruqlardan birini tanlang",
+    #     reply_markup=ReplyKeyboardMarkup(
+    #         reply_keyboard, one_time_keyboard=True, resize_keyboard=True, input_field_placeholder="Nima kerak"
+    #     ),
+    # )
+    await update.message.reply_text(f'Hello {update.effective_user.first_name}', 
+        reply_markup=buttons)
 
 async def help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text('Qanday yordam bera olaman ?')
@@ -51,7 +54,21 @@ async def weather(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
     a, b = temp.splitlines()
 
-    txt = f"Havo : {vaziyat} \nKunduzi : {a} \nKechasi : {b}"
+    txt = f"Shahar : Toshkent \nHavo : {vaziyat} \nKunduzi : {a} \nKechasi : {b}"
+    await update.message.reply_text(txt)
+
+async def weather_city(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    # site = 'https://obhavo.uz/'
+    # sms += str(update.message.text)[9:]
+    # site = requests.get(site)
+    site = requests.get('https://obhavo.uz/Nukus/')
+    soup = BeautifulSoup(site.text, 'html.parser')
+    temp = soup.find(class_ = "current-forecast").text.strip()
+    vaziyat = soup.find(class_ = "current-forecast-desc").text.strip()
+    
+    a, b = temp.splitlines()
+
+    txt = f"Shahar : Nukus \nHavo : {vaziyat} \nKunduzi : {a} \nKechasi : {b}"
     await update.message.reply_text(txt)
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -70,6 +87,8 @@ def main() -> None:
     app.add_handler(CommandHandler("help", help))
     
     app.add_handler(CommandHandler("weather", weather))
+
+    app.add_handler(MessageHandler(filters.Regex("/weather_Nukus"), weather_city))
 
     app.add_handler(MessageHandler(filters.Regex("hayr"), hayr))
 
